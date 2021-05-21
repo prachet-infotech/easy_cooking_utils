b4ac86c618ae0b7fde8f5de12dae07bc8f38d8f8
Amit Kumar <amitgarg.cse@gmail.com>
2021-05-21 15:03:58 +0530

    ToJson examples

--- bin/db-ingredient.dart
+++ bin/db-ingredient.dart
@@ -1,10 +1,37 @@
 import 'interfaces/trackable.dart';
+import 'package:meta/meta.dart';
 
 class DbIngredient {
   String uid;
   String readableKey;
   String displayNameEnglish;
-  double kgIn1liter;
-
+  double denesityInKgPerLiter;
   List<Trackable> createUpdateData;
+
+  DbIngredient({
+    @required this.uid,
+    @required this.readableKey,
+    @required this.displayNameEnglish,
+    @required this.denesityInKgPerLiter,
+    @required this.createUpdateData,
+  });
+
+  Map<String, dynamic> toDocument() {
+    return {
+      "readableKey": readableKey,
+      "displayNameEnglish": displayNameEnglish,
+      "denesityInKgPerLiter": denesityInKgPerLiter,
+      "createUpdateData": createUpdateData.map((data) => data.toJson()).toList()
+    };
+  }
+
+  factory DbIngredient.fromJson(Map<String, dynamic> json) {
+    if (json == null) {
+      return null;
+    }
+    return DbIngredient(
+      firebaseUid: json['firebaseUid'],
+      timestamp: DateTime.parse(json['timestamp']),
+    );
+  }
 }
--- bin/interfaces/trackable.dart
+++ bin/interfaces/trackable.dart
@@ -1,4 +1,25 @@
-abstract class Trackable {
+import 'package:meta/meta.dart';
+
+class Trackable {
   String firebaseUid;
   DateTime timestamp;
+
+  Trackable({
+    @required this.firebaseUid,
+    @required this.timestamp,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {'firebaseUid': firebaseUid, 'timestamp': timestamp.toString()};
+  }
+
+  factory Trackable.fromJson(Map<String, dynamic> json) {
+    if (json == null) {
+      return null;
+    }
+    return Trackable(
+      firebaseUid: json['firebaseUid'],
+      timestamp: DateTime.parse(json['timestamp']),
+    );
+  }
 }
